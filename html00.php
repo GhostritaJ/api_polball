f8410f6de9197b9d0655d1a4b0089eb5454bbaf6
GhostritaJ <149358256+GhostritaJ@users.noreply.github.com>
2023-10-30 22:35:21 +0700

    Create html00.php

---
+++ html00.php
@@ -0,0 +1,830 @@
+เลือก ลีก/ประเทศ
+
+ดูรวมทุกลีก  Argentina
+ Armenia
+ Azerbaijan
+ Brazil
+ Colombia
+ Croatia
+ Denmark
+ Ecuador
+ Egypt
+ England
+ France
+ Germany
+ Greece
+ Indonesia
+ Iraq
+ Ireland
+ Italy
+ Lithuania
+ Mexico
+ Netherland
+ Norway
+ Paraguay
+ Poland
+ Portugal
+ Romania
+ Scotland
+ Spain
+ Sweden
+ Turkey
+ USA
+ Venezuela
+ Vietnam
+32
+
+Vietnam V-League
+
+Germany 3. Liga
+
+Egypt Premier League
+
+USA MLS Major League Soccer
+
+Brazil Serie B
+
+Ecuador Serie A
+
+Brazil Serie A
+
+Venezuela Primera Division
+
+USA MLS Major League Soccer
+
+Argentina Primera Division
+
+Mexico Primera Division
+
+USA MLS Major League Soccer
+
+Mexico Primera Division
+
+Indonesia Super Liga
+
+Indonesia Super Liga
+
+Croatia 2. HNL
+
+Armenia Premier League
+
+Iraq Super League
+
+Egypt Premier League
+
+Azerbaijan Premier League
+
+Armenia Premier League
+
+Romania Liga 1
+
+Iraq Super League
+
+Lithuania A Lyga
+
+Ireland Premier Division
+
+Egypt Premier League
+
+Turkey Super Lig
+
+Poland Division 1
+
+Norway Eliteserien
+
+Italy Serie A
+
+Greece Super League
+
+Sweden Superettan
+
+Sweden Allsvenskan
+
+Portugal Segunda Liga
+
+Denmark Superliga
+
+Sweden Allsvenskan
+
+Iraq Super League
+
+Iraq Super League
+
+Romania Liga 1
+
+Argentina Primera Division
+
+Netherlands Eerste Divisie
+
+Greece Super League
+
+Scotland Cup
+
+Italy Serie A
+
+France Ligue 2
+
+Spain LaLiga2
+
+Spain LaLiga
+
+Norway Eliteserien
+
+England Championship
+
+Portugal Primeira Liga
+
+Paraguay Primera Division
+
+Colombia Torneo Aguila
+
+Argentina Primera Division
+
+Argentina Primera Division
+
+USA MLS Major League Soccer
+
+Brazil Serie B
+
+Paraguay Primera Division
+
+57
+
+Sun 19:15 - LIVE 82
+
+Sun 19:30
+
+Sun 21:00
+
+Mon 05:00 - LIVE 90+5
+
+Mon 05:00 - LIVE 90+8
+
+Mon 06:00 - FT
+
+Mon 06:00 - FT
+
+Mon 06:30 - FT
+
+Mon 07:00 - LIVE 75
+
+Mon 07:00 - LIVE 82
+
+Mon 08:05 - LIVE 35
+
+Mon 09:00
+
+Mon 10:00
+
+Mon 15:00
+
+Mon 19:00
+
+Mon 20:00
+
+Mon 20:00
+
+Mon 21:00
+
+Mon 21:00
+
+Mon 22:00
+
+Mon 22:00
+
+Mon 22:30
+
+Mon 23:00
+
+Mon 23:00
+
+Mon 23:45
+
+Tue 00:00
+
+Tue 00:00
+
+Tue 00:00
+
+Tue 00:30
+
+Tue 00:30
+
+Tue 00:30
+
+Tue 01:00
+
+Tue 01:00
+
+Tue 01:00
+
+Tue 01:00
+
+Tue 01:10
+
+Tue 01:30
+
+Tue 01:30
+
+Tue 01:30
+
+Tue 02:00
+
+Tue 02:00
+
+Tue 02:30
+
+Tue 02:45
+
+Tue 02:45
+
+Tue 02:45
+
+Tue 03:00
+
+Tue 03:00
+
+Tue 03:00
+
+Tue 03:00
+
+Tue 03:15
+
+Tue 04:00
+
+Tue 04:00
+
+Tue 04:30
+
+Tue 05:00
+
+Tue 06:00
+
+Tue 06:00
+
+Tue 06:30
+
+57
+
+Ha Noi
+
+saarbrücken
+
+El Geish
+
+Houston Dynamo
+
+Vitória
+
+Orense
+
+โบตาโฟโก้
+
+Academia Puerto Cabello
+
+Cincinnati
+
+Independiente
+
+Santos Laguna
+
+St. Louis City
+
+Tijuana
+
+RANS Nusantara
+
+Bali United
+
+Jarun
+
+Noah
+
+Al-Qasim
+
+El Daklyeh
+
+Araz
+
+Urartu
+
+Botoşani
+
+Naft Al-Wasat
+
+FA Šiauliai
+
+Cork City
+
+National Bank of Egypt
+
+Beşiktaş
+
+Znicz Pruszków
+
+Bodø / Glimt
+
+เอ็มโปลี
+
+AEK เอเธนส์
+
+GIF Sundsvall
+
+Hammarby
+
+Vilaverdense
+
+AGF
+
+IFK Göteborg
+
+Newroz SC
+
+Al Shorta
+
+CFR Cluj
+
+Talleres Córdoba
+
+ยอง อูเทรค
+
+OFI
+
+Musselburgh Athletic
+
+ลาซิโอ
+
+แซง เอเตียน
+
+Burgos
+
+กรานาดา
+
+Molde
+
+โคเวนทรี ซิตี้
+
+เบาวิสต้า
+
+Sportivo Luqueño
+
+Cúcuta Deportivo
+
+Lanús
+
+Sarmiento
+
+Orlando City
+
+Chapecoense
+
+Libertad
+
+57
+
+2 : 3
+
+0 : 0
+
+0 : 0
+
+2 : 1
+
+0 : 0
+
+2 : 3
+
+0 : 1
+
+0 : 1
+
+2 : 0
+
+0 : 0
+
+1 : 1
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+0 : 0
+
+57
+
+Hai Phong
+
+ดินาโม เดรสเดน
+
+Ceramica Cleopatra
+
+Real Salt Lake
+
+Juventude
+
+Barcelona Guayaquil
+
+Cuiabá
+
+Deportivo Táchira
+
+New York RB
+
+Arsenal de Sarandi
+
+Juárez
+
+Sporting KC
+
+Atlas
+
+PSM
+
+Persita
+
+Zrinski Jurjevac
+
+Alashkert
+
+Erbil
+
+Ismaily
+
+Qarabağ
+
+Shirak
+
+Universitatea Cluj
+
+Al Quwa Al Jawiya
+
+Panevėžys
+
+Shamrock Rovers
+
+Future FC
+
+Gaziantep F.K.
+
+Miedź legnica
+
+Lillestrøm
+
+อตาลันต้า
+
+พีเอโอเค
+
+Örgryte
+
+Sirius
+
+Porto II
+
+แรนเดอร์ส
+
+Elfsborg
+
+Duhok
+
+Karbala
+
+SSC Farul
+
+Banfield
+
+เอ็มวีวี มาสทริคท์
+
+Olympiakos Piraeus
+
+Clyde
+
+ฟิออเรนติน่า
+
+อองเช่ร์
+
+เรอัล ซาราโกซ่า
+
+บียาเรอัล
+
+Tromsø
+
+เวสต์บรอมวิช อัลเบียน
+
+สปอร์ติ้ง ลิสบอน
+
+Sportivo Trinidense
+
+Barranquilla
+
+Belgrano
+
+San Lorenzo
+
+Nashville SC
+
+Tombense
+
+Resistencia
+
+57
+
+
+---
+doomunde : 4kball.com ---
+rb88HD : ballnomhok.com ---
+doo90min : 90minlive.com ---
+nondooball : nondooball.com ---
+---
+Ballsodtv : warpdooball.net ---
+KickoffHD : aisplay24hd.com ---
+KickoffHD : true24hd.net ---
+doomunde : 4kball.com ---
+rb88HD : ballnomhok.com ---
+dooballsod69 : dooballx.com ---
+ufa147sport : thdooball.com ---
+VvipScore : vvipscore.com ---
+DuballHD4K : duball24hrs.com ---
+doo90min : 90minlive.com ---
+nondooball : nondooball.com ---
+---
+Ballsodtv : warpdooball.net ---
+KickoffHD : aisplay24hd.com ---
+KickoffHD : true24hd.net ---
+doomunde : 4kball.com ---
+rb88HD : ballnomhok.com ---
+ufa189hd : zonedooball.com ---
+doballthailand : xn--l3caqb0aylm5a2a7gub1fxe.com ---
+VvipScore : vvipscore.com ---
+DuballHD4K : duball24hrs.com ---
+doo90min : 90minlive.com ---
+nondooball : nondooball.com ---
+---
+Ballsodtv : warpdooball.net ---
+KickoffHD : aisplay24hd.com ---
+KickoffHD : true24hd.net ---
+doballthailand : xn--l3caqb0aylm5a2a7gub1fxe.com ---
+dooball356 : duball356.com ---
+dooballsod69 : dooballx.com ---
+ufa147sport : thdooball.com ---
+VvipScore : vvipscore.com ---
+DuballHD4K : duball24hrs.com ---
+---
+Ballsodtv : warpdooball.net ---
+KickoffHD : aisplay24hd.com ---
+KickoffHD : true24hd.net ---
+doomunde : 4kball.com ---
+rb88HD : ballnomhok.com ---
+ufa189hd : zonedooball.com ---
+doballthailand : xn--l3caqb0aylm5a2a7gub1fxe.com ---
+VvipScore : vvipscore.com ---
+paperj : yedhe.com ---
+DuballHD4K : duball24hrs.com ---
+doo90min : 90minlive.com ---
+FIN99.NET : fin99live.com ---
+nondooball : nondooball.com ---
+---
+Ballsodtv : warpdooball.net ---
+KickoffHD : aisplay24hd.com ---
+KickoffHD : true24hd.net ---
+doballthailand : xn--l3caqb0aylm5a2a7gub1fxe.com ---
+ufa147sport : thdooball.com ---
+VvipScore : vvipscore.com ---
+DuballHD4K : duball24hrs.com ---
+---
+---
+---
+---
+---
+---
+---
+---
+---
+---
+---
+---
+---
+---
+FIN99.NET : fin99live.com ---
+---
+---
+---
+---
+---
+FIN99.NET : fin99live.com ---
+---
+---
+---
+footbailcom : ball.soodaza.com ---
+FIN99.NET : fin99live.com ---
+---
+---
+---
+---
+---
+FIN99.NET : fin99live.com ---
+---
+---
+---
+---
+FIN99.NET : fin99live.com ---
+---
+---
+---
+---
+---
+footbailcom : ball.soodaza.com ---
+FIN99.NET : fin99live.com ---
+---
+---
+---
+footbailcom : ball.soodaza.com ---
+FIN99.NET : fin99live.com ---
+---
+---
+FIN99.NET : fin99live.com ---
+---
+FIN99.NET : fin99live.com ---
+---
+---
+---
+---
+---
+---
+---
+---
+
+
+
+
+
+
+
+
+
+
+57https://www.4kball.com/stream.php?id=132730
+https://www.ballnomhok.com/stream.php?id=132730
+https://xn--l3caqb0aylm5a2a7gub1fxe.com/live-football-ดูบอลสด/?id=2243700
+https://www.90minlive.com/stream.php?id=132730
+https://www.nondooball.com/stream.php?id=132730
+https://www.4kball.com/stream.php?id=132749
+https://www.ballnomhok.com/stream.php?id=132749
+https://www.90minlive.com/stream.php?id=132749
+https://www.nondooball.com/stream.php?id=132749
+https://warpdooball.net/live-football/?id=2243945
+https://aisplay24hd.com/live-football/?id=2243945
+https://true24hd.net/match/?id=2243945
+https://www.4kball.com/stream.php?id=132819
+https://www.ballnomhok.com/stream.php?id=132819
+https://dooballx.com/match/?id=2243945
+https://thdooball.com/live-football/?id=2243945
+https://vvipscore.com/match?id=2243945
+https://www.duball24hrs.com/live-football/?id=2243945
+https://www.90minlive.com/stream.php?id=132819
+https://www.nondooball.com/stream.php?id=132819
+https://warpdooball.net/live-football/?id=2243946
+https://aisplay24hd.com/live-football/?id=2243946
+https://true24hd.net/match/?id=2243946
+https://www.4kball.com/stream.php?id=132820
+https://www.ballnomhok.com/stream.php?id=132820
+https://zonedooball.com/live-football/?id=2243946
+https://xn--l3caqb0aylm5a2a7gub1fxe.com/live-football-ดูบอลสด/?id=2243941
+https://vvipscore.com/match?id=2243946
+https://www.duball24hrs.com/live-football/?id=2243946
+https://www.90minlive.com/stream.php?id=132820
+https://www.nondooball.com/stream.php?id=132820
+https://warpdooball.net/live-football/?id=2243947
+https://aisplay24hd.com/live-football/?id=2243947
+https://true24hd.net/match/?id=2243947
+https://xn--l3caqb0aylm5a2a7gub1fxe.com/live-football-ดูบอลสด/?id=2243942
+https://duball356.com/live-football/?id=2243947
+https://dooballx.com/match/?id=2243947
+https://thdooball.com/live-football/?id=2243947
+https://vvipscore.com/match?id=2243947
+https://www.duball24hrs.com/live-football/?id=2243947
+https://warpdooball.net/live-football/?id=2243948
+https://aisplay24hd.com/live-football/?id=2243948
+https://true24hd.net/match/?id=2243948
+https://www.4kball.com/stream.php?id=132821
+https://www.ballnomhok.com/stream.php?id=132821
+https://zonedooball.com/live-football/?id=2243948
+https://xn--l3caqb0aylm5a2a7gub1fxe.com/live-football-ดูบอลสด/?id=2243944
+https://vvipscore.com/match?id=2243948
+https://yedhe.com/doballsod/view/27662
+https://www.duball24hrs.com/live-football/?id=2243948
+https://www.90minlive.com/stream.php?id=132821
+https://fin99live.com/fixtures/18791330
+https://www.nondooball.com/stream.php?id=132821
+https://warpdooball.net/live-football/?id=2243949
+https://aisplay24hd.com/live-football/?id=2243949
+https://true24hd.net/match/?id=2243949
+https://xn--l3caqb0aylm5a2a7gub1fxe.com/live-football-ดูบอลสด/?id=2243949
+https://thdooball.com/live-football/?id=2243949
+https://vvipscore.com/match?id=2243949
+https://www.duball24hrs.com/live-football/?id=2243949
+https://fin99live.com/fixtures/18859569
+https://fin99live.com/fixtures/18903638
+https://ball.soodaza.com/match/2419621
+https://fin99live.com/fixtures/18867400
+https://fin99live.com/fixtures/18841544
+https://fin99live.com/fixtures/18859570
+https://ball.soodaza.com/match/2419625
+https://fin99live.com/fixtures/18867404
+https://ball.soodaza.com/match/2404401
+https://fin99live.com/fixtures/18848031
+https://fin99live.com/fixtures/18846895
+https://fin99live.com/fixtures/18870389
+
+
+57
+
+72
+
+ไม่สามารถแปลงข้อมูล JSON ได้
+Fatal error: Uncaught Error: Call to a member function prepare() on null in C:\xampp\htdocs\api-polball\dbconfig.php:44 Stack trace: #0 C:\xampp\htdocs\api-polball\index.php(11): Database->query('SELECT * FROM p...') #1 {main} thrown in C:\xampp\htdocs\api-polball\dbconfig.php on line 44
